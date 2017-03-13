5f50e46235d4bfbe4cbc2b3cf3bf0dfde08d1913
evan miller <emille4@clemson.edu>
2017-03-13 13:34:52 -0400

    added login route

--- app/scripts/router.js
+++ app/scripts/router.js
@@ -3,14 +3,22 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 var HomeContainer = require('./components/home.jsx').HomeContainer;
+var LoginContainer = require('./components/login.jsx').LoginContainer;
 
 var AppRouter = Backbone.Router.extend({
   routes: {
     '': 'index',
+    'login': 'login',
   },
   index: function(){
     ReactDOM.render(
       React.createElement(HomeContainer),
+      document.getElementById('app')
+    )
+  },
+  login: function(){
+    ReactDOM.render(
+      React.createElement(LoginContainer),
       document.getElementById('app')
     )
   }

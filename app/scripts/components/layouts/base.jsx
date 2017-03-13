131b5ae2a3523b33a4cceb9ba29abc72e860e443
evan miller <emille4@clemson.edu>
2017-03-13 15:14:41 -0400

    added seperate containers and moved navbar

--- app/scripts/components/layouts/base.jsx
+++ app/scripts/components/layouts/base.jsx
@@ -3,12 +3,12 @@ var React = require('react');
 class BaseLayout extends React.Component {
   render(){
     return(
-      <div className="container-fluid">
-        <HeaderNav />
+      <div>
         <Banner />
-
-        {this.props.children}
-        
+        <HeaderNav />
+          <div className="container">
+            {this.props.children}
+          </div>
         <Footer />
       </div>
     )
@@ -18,11 +18,13 @@ class BaseLayout extends React.Component {
 class HeaderNav extends React.Component{
   render(){
     return(
-      <div className="row">
-        <nav className="home-nav-contain navbar">
-          <a className="navbar-brand" href="#">Navbar</a>
-          <div className="home-nav-contain">
-            <ul className="home-nav navbar-nav">
+      <div className="container">
+        <div className="row">
+          <nav className="navbar">
+            <ul className="home-nav">
+              <li>
+                <a className="navbar-brand" href="#">Navbar</a>
+              </li>
               <li>
                 <a href="#">How it Works</a>
               </li>
@@ -30,8 +32,8 @@ class HeaderNav extends React.Component{
                 <a href="#" className="btn btn-primary" role="button">Signup</a>
               </li>
             </ul>
-          </div>
-        </nav>
+          </nav>
+        </div>
       </div>
     )
   }
@@ -40,10 +42,12 @@ class HeaderNav extends React.Component{
 class Banner extends React.Component{
   render(){
     return(
-      <div className="row">
-        <div className="jumbotron">
-          <div className="mission-statement">
-            <h1>Mission Statement</h1>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="jumbotron">
+            <div className="mission-statement">
+              <h1>Mission Statement</h1>
+            </div>
           </div>
         </div>
       </div>
@@ -54,19 +58,21 @@ class Banner extends React.Component{
 class Footer extends React.Component{
   render(){
     return(
-      <footer>
-        <div className="footer-copyright">
-          <span>App Name,</span>
-          <span>an app built by <a href="#">Evan Miller</a></span>
+      <div clasName="container">
+        <div className="row footer">
+          <div className="footer-copyright">
+            <span>App Name,</span>
+            <span>an app built by <a href="#">Evan Miller</a></span>
+          </div>
+          <div className="footer-nav">
+            <ul>
+              <li><a href="#">Home</a></li>
+              <li><a href="#">Signup</a></li>
+              <li><a href="#">Write Review</a></li>
+            </ul>
+          </div>
         </div>
-        <div className="footer-nav">
-          <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Signup</a></li>
-            <li><a href="#">Write Review</a></li>
-          </ul>
-        </div>
-      </footer>
+      </div>
     )
   }
 }

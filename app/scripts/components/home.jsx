2b99f74d8991119ed34b8e2f39713de0f2ec3328
evan miller <emille4@clemson.edu>
2017-03-13 13:33:56 -0400

    added base layout to homeContainer and broke home container into sub components

--- app/scripts/components/home.jsx
+++ app/scripts/components/home.jsx
@@ -1,31 +1,11 @@
 var React = require('react');
 
+var BaseLayout = require('./layouts/base.jsx').BaseLayout;
+
 class HomeContainer extends React.Component {
   render(){
     return(
-      <div className="container-fluid">
-        <div className="row">
-          <nav className="home-nav-contain navbar">
-            <a className="navbar-brand" href="#">Navbar</a>
-            <div className="home-nav-contain">
-              <ul className="home-nav navbar-nav">
-                <li>
-                  <a href="#">How it Works</a>
-                </li>
-                <li>
-                  <a href="#" className="btn btn-primary" role="button">Signup</a>
-                </li>
-              </ul>
-            </div>
-          </nav>
-        </div>
-        <div className="row">
-          <div className="jumbotron">
-            <div className="mission-statement">
-              <h1>Mission Statement</h1>
-            </div>
-          </div>
-        </div>
+      <BaseLayout>
         <div className="row">
           <div className="col-sm-6 col-md-4">
             <div className="thumbnail">
@@ -58,20 +38,8 @@ class HomeContainer extends React.Component {
             </div>
           </div>
         </div>
-        <footer>
-          <div className="footer-copyright">
-            <span>App Name,</span>
-            <span>an app built by <a href="#">Evan Miller</a></span>
-          </div>
-          <div className="footer-nav">
-            <ul>
-              <li><a href="#">Home</a></li>
-              <li><a href="#">Signup</a></li>
-              <li><a href="#">Write Review</a></li>
-            </ul>
-          </div>
-        </footer>
-      </div>
+      </BaseLayout>
+
     );
   }
 }

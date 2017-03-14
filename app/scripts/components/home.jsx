d5d37234963fdce7a220b370f6d9be092bb8c817
evan miller <emille4@clemson.edu>
2017-03-14 10:57:57 -0400

    added home layout with banner, took banner out of base layout

--- app/scripts/components/home.jsx
+++ app/scripts/components/home.jsx
@@ -1,44 +1,46 @@
 var React = require('react');
 
-var BaseLayout = require('./layouts/base.jsx').BaseLayout;
+var HomeLayout = require('./layouts/home_layout.jsx').HomeLayout;
 
 class HomeContainer extends React.Component {
   render(){
     return(
-      <BaseLayout>
-        <div className="row">
-          <div className="col-sm-6 col-md-4">
-            <div className="thumbnail">
-              <img src="https://unsplash.it/400/?random" alt="..." />
-              <div className="caption">
-                <h3>Write Review</h3>
-                <p>Write a review to help potential students or current students better understand the value of the degree they are or will be earning.</p>
-                <p><a href="#" className="btn btn-primary" role="button">Write Review</a></p>
+      <HomeLayout>
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-6 col-md-4">
+              <div className="thumbnail">
+                <img src="https://unsplash.it/400/?random" alt="..." />
+                <div className="caption">
+                  <h3>Write Review</h3>
+                  <p>Write a review to help potential students or current students better understand the value of the degree they are or will be earning.</p>
+                  <p><a href="#" className="btn btn-primary" role="button">Write Review</a></p>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="col-sm-6 col-md-4">
-            <div className="thumbnail">
-              <img src="https://unsplash.it/400/?random" alt="..." />
-              <div className="caption">
-                <h3>Read Reviews</h3>
-                <p>Discover the value of different degrees from real people who have earned earned the degree.</p>
-                <p><a href="#" className="btn btn-primary" role="button">Read Reviews</a></p>
+            <div className="col-sm-6 col-md-4">
+              <div className="thumbnail">
+                <img src="https://unsplash.it/400/?random" alt="..." />
+                <div className="caption">
+                  <h3>Read Reviews</h3>
+                  <p>Discover the value of different degrees from real people who have earned earned the degree.</p>
+                  <p><a href="#" className="btn btn-primary" role="button">Read Reviews</a></p>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="col-sm-6 col-md-4">
-            <div className="thumbnail">
-              <img src="https://unsplash.it/400/?random" alt="..." />
-              <div className="caption">
-                <h3>View Degree Information</h3>
-                <p>View the statistics for different college degrees including salary by major.</p>
-                <p><a href="#" className="btn btn-primary" role="button">View Statistics</a></p>
+            <div className="col-sm-6 col-md-4">
+              <div className="thumbnail">
+                <img src="https://unsplash.it/400/?random" alt="..." />
+                <div className="caption">
+                  <h3>View Degree Information</h3>
+                  <p>View the statistics for different college degrees including salary by major.</p>
+                  <p><a href="#" className="btn btn-primary" role="button">View Statistics</a></p>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </BaseLayout>
+      </HomeLayout>
 
     );
   }

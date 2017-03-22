c859f5c24bf9e57e4971af5100b862cb75863734
evan miller <emille4@clemson.edu>
2017-03-22 15:55:27 -0400

    selecting  dynamically renders major in url

--- app/scripts/components/degree_detail.jsx
+++ app/scripts/components/degree_detail.jsx
@@ -1,58 +1,69 @@
 var React = require('react');
 
 var User = ('../../models/user').User;
+var DegreeCollection = require('../models/degree.js').DegreeCollection;
 
 var BaseLayout = require('./layouts/base.jsx').BaseLayout;
 
+
 class DegreeDetailContainer extends React.Component{
+  constructor(props){
+    super(props);
+
+    var degree = new DegreeCollection()
+
+    this.state = {
+      degree
+    }
+  }
   render(){
+    var degree = this.state.degree;
+    console.log(degree)
     return(
       <BaseLayout>
-        <div className="container">
-          <div className="row">
-            <div className="col-sm-12 degree-detail-contain">
-              <div className="degree-detail-title">
-                <h1>Average Salary</h1>
-                <p>of top 20 schools</p>
-              </div>
-              <div className="row">
-                <div className="col-sm-4">
-                  <div className="program-level certificate-level">
-                    <h4>Certificate</h4>
-                  </div>
-                  <div className="avg-salary certificate-salary">
-                    <h5>Avg Salary</h5>
-                  </div>
+        <div className="row">
+          <div className="col-sm-12 degree-detail-contain">
+            <div className="degree-detail-title">
+              <h1>Average Salary</h1>
+              <p>of top 20 schools</p>
+            </div>
+            <div className="row">
+              <div className="col-sm-4">
+                <div className="program-level certificate-level">
+                  <h4>Certificate</h4>
                 </div>
-                <div className="col-sm-4">
-                <div className="program-level associate-level">
-                  <h4>Associate's</h4>
-                </div>
-                <div className="avg-salary associate-salary">
+                <div className="avg-salary certificate-salary">
                   <h5>Avg Salary</h5>
                 </div>
               </div>
               <div className="col-sm-4">
-                <div className="program-level bachelor-level">
-                  <h4>Bachelor's</h4>
-                </div>
-                <div className="avg-salary bachelor-salary">
-                  <h5>Avg Salary</h5>
-                </div>
+              <div className="program-level associate-level">
+                <h4>Associate's</h4>
               </div>
+              <div className="avg-salary associate-salary">
+                <h5>Avg Salary</h5>
               </div>
-              <div className="job-satisfaction-contain">
-                <div className="job-satisfaction">
-                  <label>Job Satisfaction: </label>
-                  <span>rating</span>
-                </div>
-                <div className="rate">
-                  <label>Rate: </label>
-                  <span>Clickable star rating system</span>
-                </div>
-                <div className="degree-select-nav">
-                  <a href="#degree/">Select a different Degree</a>
-                </div>
+            </div>
+            <div className="col-sm-4">
+              <div className="program-level bachelor-level">
+                <h4>Bachelor's</h4>
+              </div>
+              <div className="avg-salary bachelor-salary">
+                <h5>Avg Salary</h5>
+              </div>
+            </div>
+            </div>
+            <div className="job-satisfaction-contain">
+              <div className="job-satisfaction">
+                <label>Job Satisfaction: </label>
+                <span>rating</span>
+              </div>
+              <div className="rate">
+                <label>Rate: </label>
+                <span>Clickable star rating system</span>
+              </div>
+              <div className="degree-select-nav">
+                <a href="#degree/">Select a different Degree</a>
               </div>
             </div>
           </div>
--- app/scripts/components/degree_select.jsx
+++ app/scripts/components/degree_select.jsx
@@ -11,22 +11,46 @@ class DegreeSelectContainer extends React.Component{
 
     var degreeCollection = new DegreeCollection;
 
+    this.updateDegreeDetail = this.updateDegreeDetail.bind(this);
+    this.updateSelection = this.updateSelection.bind(this);
+    // this.handleSubmit = this.handleSubmit.bind(this);
+
     this.state = {
       degreeCollection,
       selectedMajor: null
     }
   }
-  updateDegreeDetail() {
-    var degreeCollection = this.state.degreeCollection;
-    var selectedMajor = this.state.selectedMajor;
 
-    degreeCollection.urlSetter(2, {selectedMajor});
-    degreeCollection.fetch().then(() =>
-    Backbone.history.navigate('#degree/detail/', {trigger: true})
-    )
+  updateSelection(e){
+    e.preventDefault();
+    var selectedMjr = e.target.value;
+
+    this.setState({selectedMajor: selectedMjr});
+
+    this.updateDegreeDetail(selectedMjr);
   }
+
+  updateDegreeDetail(selected) {
+    var selectedMajor = selected;
+    var degreeCollection = this.state.degreeCollection;
+    var associatesUrl =   degreeCollection.urlSetter(2, selectedMajor);
+
+    associatesUrl.fetch().done((response) => {
+      this.setState({degreeCollection: degreeCollection});
+      console.log('url', degreeCollection)
+    });
+  }
+  // handleSubmit(e){
+  //   e.preventDefault();
+  //   var selectedMajor = this.state.selectedMajor;
+  //
+  //   this.updateDegreeDetail(this.state)
+  // }
   render(){
 
+    var programs = Object.keys(programNames).map(function(key, index){
+      return <option key={programNames[key]} value={key}>{programNames[key]}</option>
+    });
     return(
       <BaseLayout>
         <div className="row">
@@ -36,9 +60,25 @@ class DegreeSelectContainer extends React.Component{
               <h1>Find Degree</h1>
               </div>
               <div className="degree-select-form">
-                <DegreeSelect
-                  updateDegreeDetail={this.updateDegreeDetail}
-                />
+                <div>
+                  <div className="degree-select">
+                  <span className="degree-select-label">
+                    Select Your Degree:
+                  </span>
+                  <select
+                    onChange={this.updateSelection}
+                    value={this.state.selectedMajor}
+                    className="degree-select option1" name="degree-select">
+                    <option value="Degree">Select Degree Program</option>
+                      {programs}
+                  </select>
+                  </div>
+                  <div className="view-salary-btn">
+                    <a className="btn btn-success" href="#degree/detail/" role="button">
+                      View Salary
+                    </a>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
@@ -48,52 +88,58 @@ class DegreeSelectContainer extends React.Component{
   }
 }
 
-class DegreeSelect extends React.Component{
-  constructor(props){
-    super(props);
-
-    this.updateSelection = this.updateSelection.bind(this);
-
-    this.state = {
-      selectedMajor: ''
-    }
-  }
-  updateSelection(e){
-      this.setState({selectedMajor: e.target.value});
-  }
-  handleSubmit(e){
-    e.preventDefault();
-    var selectedMajor = this.state.selectedMajor;
-
-    this.props.updateDegreeDetail(this.state)
-  }
-  render(){
-    var programs = Object.keys(programNames).map(function(key, index){
-      return <option key={programNames[key]} value={key}>{programNames[key]}</option>
-    });
-
-    return(
-      <form onSubmit={this.handleSubmit}>
-        <div className="degree-select">
-        <span className="degree-select-label">
-          Select Your Degree:
-        </span>
-        <select
-          onChange={this.updateSelection}
-          className="degree-select option1" name="degree-select">
-          <option value="Degree">Select Degree Program</option>
-          {programs}
-        </select>
-        </div>
-        <div className="view-salary-btn">
-          <a className="btn btn-success" href="#degree/detail/" role="button">
-            View Salary
-          </a>
-        </div>
-      </form>
-    )
-  }
-}
+// class DegreeSelect extends React.Component{
+//   constructor(props){
+//     super(props);
+//
+//     this.updateSelection = this.updateSelection.bind(this);
+//     this.handleSubmit = this.handleSubmit.bind(this);
+//
+//     this.state = {
+//       selectedMajor: ''
+//     }
+//   }
+//
+//   updateSelection(e){
+//       this.setState({selectedMajor: e.target.value})
+//       // this.props.updateDegreeDetail(e.target.value)
+//   }
+//
+//   handleSubmit(e){
+//     e.preventDefault();
+//     var selectedMajor = this.state.selectedMajor;
+//
+//     this.props.updateDegreeDetail(this.state)
+//   }
+//
+//   render(){
+//     var programs = Object.keys(programNames).map(function(key, index){
+//       return <option key={programNames[key]} value={key}>{programNames[key]}</option>
+//     });
+//
+//     return(
+//       <form onSubmit={this.handleSubmit}>
+//         <div className="degree-select">
+//         <span className="degree-select-label">
+//           Select Your Degree:
+//         </span>
+//         <select
+//           onChange={this.updateSelection}
+//           value={this.state.selectedMajor}
+//           className="degree-select option1" name="degree-select">
+//           <option value="Degree">Select Degree Program</option>
+//           {programs}
+//         </select>
+//         </div>
+//         <div className="view-salary-btn">
+//           <a className="btn btn-success" href="#degree/detail/" role="button">
+//             View Salary
+//           </a>
+//         </div>
+//       </form>
+//     )
+//   }
+// }
 
 module.exports = {
   DegreeSelectContainer

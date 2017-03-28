83fb1c914ad3095220cb6c53b1dbc864696670e4
evan miller <emille4@clemson.edu>
2017-03-28 15:00:20 -0400

    review form is updating, fixed bugs with checkbox

--- app/scripts/components/review_create_edit.jsx
+++ app/scripts/components/review_create_edit.jsx
@@ -51,7 +51,7 @@ class ReviewCreateEditContainer extends React.Component {
     this.handleSubmit = this.handleSubmit.bind(this);
   }
   updateDegree(e){
-    console.log('e', e.target.value);
+    // console.log('e', e.target.value);
     this.setState({ degree: e.target.value })
     // this.state.review.set({ degree: e.target.value });
   }
@@ -60,11 +60,7 @@ class ReviewCreateEditContainer extends React.Component {
     // this.state.review.set({ major: e.target.value });
   }
   updateEmployment(e){
-    if(e.target.value == 'on') {
-      this.setState({ employment: 'Yes' })
-    } else {
-      this.setState({ employment: 'No' })
-    }
+    this.setState({ employment: e.target.value })
     // this.state.review.set({ employment: e.target.value == 'on' });
 
   }
@@ -77,12 +73,12 @@ class ReviewCreateEditContainer extends React.Component {
     // this.state.review.set({ salary: e.target.value });
   }
   updateRecommend(e){
-    if(e.target.value == 'on') {
+    if(e.target.value == 'yes') {
       this.setState({ recommend: 'Yes' })
     } else {
       this.setState({ recommend: 'No' })
     }
-    // this.state.review.set({ recommend: e.target.value });
+    this.state.review.set({ recommend: e.target.value });
   }
   handleShowOptions(e){
     e.preventDefault();
@@ -108,99 +104,107 @@ class ReviewCreateEditContainer extends React.Component {
     });
   }
   render() {
-    console.log('current degree:', this.state.degree)
-    console.log('current major:', this.state.major)
+
     var programs = Object.keys(programNames).map(function(key, index){
       return <option key={key} value={programNames[key]}>{programNames[key]}</option>
     });
     return (
       <BaseLayout>
-        <div className="review-form-contain">
-          <div className="review-form-title">
-            <h1>Leave A Review of Your Degree Below</h1>
-          </div>
-          <form onSubmit={ this.handleSubmit }>
-            <div className="row">
-              <div className="col-sm-8 col-sm-offset-2">
+        <div className="row">
+          <div className="col-sm-8 col-sm-offset-2">
+            <div className="review-form-contain">
+              <div className="review-form-title">
+                <h1>Leave A Review of Your Degree Below</h1>
+              </div>
+              <form onSubmit={ this.handleSubmit }>
                 <div className="select-degree-contain">
-                  <div>
-                  <label className="degree-select-label">
-                    Select Your Degree and Major:
-                  </label>
+                  <div className="select-degree-title">
+                    <label className="degree-select-label">
+                      Select Your Degree and Major:
+                    </label>
                   </div>
                   <select onChange={ this.updateDegree } className="select-degree option1" name="select-degree" value={this.state.degree}>
                     <option id="select-deg" value="select-degree">Select Degree</option>
                     <option id="associates" value="associates">Associates</option>
                     <option id="bachelors" value="bachelors">Bachelors</option>
                   </select>
-                  <select onChange={ this.updateMajor } className="select-major option1" name="select-major"  value={this.state.major}>
+                  <select onChange={ this.updateMajor } className="select-major option1" name="select-major"  value={ this.state.major }>
                     <option id="select-maj" value="select-major">Select Major</option>
                     {programs}
                   </select>
                 </div>
-                <div className="checkbox">
-                  <h5>Were you able to get a job in the field of this degree?</h5>
-                  <label>
-                    <input onChange={ this.updateEmployment }  type="checkbox" checked={this.state.employment === 'Yes' ? true : false}/>
-                    Yes
-                  </label>
-                  <label>
-                    <input onChange={ this.updateEmployment }  type="checkbox" checked={this.state.employment === 'No' ? true : false}/>
-                    No
+                <div className="form-group">
+                  <label htmlFor="employment">
+                    What type of job did you get with this degree?
+                    <input onChange={ this.updateEmployment} value={ this.state.employment } className="form-control" type="text" placeholder="Quality Engineer, Manager, etc."/>
                   </label>
                 </div>
-                <a onClick={ this.handleShowOptions }>Next</a>
-                { this.state.showOptions ? <GotAJobForm
-                  experience={ this.state.experience }
-                  salary={ this.state.salary }
-                  recommend={ this.state.recommend }
-                  updateExperience={ this.updateExperience }
-                  updateSalary={ this.updateSalary }
-                  updateRecommend={ this.updateRecommend }
-                /> : null }
-
+                <div className="form-group">
+                  <label htmlFor="experience">
+                    Number of years working in this field:
+                    <input onChange={ this.updateExperience } value={ this.state.experience } className="form-control" type="text" placeholder="years"/>
+                  </label>
+                </div>
+                <div className="form-group">
+                  <label htmlFor="salary">
+                    Salary:
+                    <input onChange={ this.updateSalary} value={ this.state.salary } className="form-control" type="text" placeholder="$0.00"/>
+                  </label>
+                </div>
+                <div className="checkbox">
+                  <label>
+                    <input onChange={ this.updateRecommend } value="yes" type="checkbox" checked={ this.state.recommend === 'Yes' ? true : false } />
+                    Recommend
+                  </label>
+                  <label>
+                    <input onChange={ this.updateRecommend } value="no" type="checkbox" checked={ this.state.recommend === 'No' ? true : false } />
+                    Don't Recommend
+                  </label>
+                </div>
                 <div className="review-submit pull-right">
                   <input className="btn btn-primary" type="submit" value="Submit Review"/>
                 </div>
-              </div>
+              </form>
+
             </div>
-          </form>
+          </div>
         </div>
       </BaseLayout>
     )
   }
 }
 
-class GotAJobForm extends React.Component {
-  render(){
-    return(
-      <div className="got-job-form">
-        <div className="form-group">
-          <label htmlFor="experience">
-            Number of years working in this field:
-            <input onChange={ this.props.updateExperience } value={ this.props.experience } className="form-control" type="text" placeholder="years"/>
-          </label>
-        </div>
-        <div className="form-group">
-          <label htmlFor="salary">
-            Salary:
-            <input onChange={ this.props.updateSalary} value={ this.props.salary } className="form-control" type="text" placeholder="$0.00"/>
-          </label>
-        </div>
-        <div className="checkbox">
-          <label>
-            <input onChange={ this.props.updateRecommend }  type="checkbox" checked={ this.props.recommend === 'Yes' ? true : false } />
-            Recommend
-          </label>
-          <label>
-            <input onChange={ this.props.updateEmployment }  type="checkbox" checked={ this.props.recommend === 'No' ? true : false } />
-            Don't Recommend
-          </label>
-        </div>
-      </div>
-    )
-  }
-}
+// class GotAJobForm extends React.Component {
+//   render(){
+//     return(
+//       <div className="got-job-form">
+//         <div className="form-group">
+//           <label htmlFor="experience">
+//             Number of years working in this field:
+//             <input onChange={ this.props.updateExperience } value={ this.props.experience } className="form-control" type="text" placeholder="years"/>
+//           </label>
+//         </div>
+//         <div className="form-group">
+//           <label htmlFor="salary">
+//             Salary:
+//             <input onChange={ this.props.updateSalary} value={ this.props.salary } className="form-control" type="text" placeholder="$0.00"/>
+//           </label>
+//         </div>
+//         <div className="checkbox">
+//           <label>
+//
+//             <input value="yes" onChange={ this.props.updateRecommend }  type="checkbox" checked={ this.props.recommend === 'Yes' ? true : false } />
+//             Recommend
+//           </label>
+//           <label>
+//             <input value="no" onChange={ this.props.updateEmployment }  type="checkbox" checked={ this.props.recommend === 'No' ? true : false } />
+//             Don't Recommend
+//           </label>
+//         </div>
+//       </div>
+//     )
+//   }
+// }
 
 module.exports = {
   ReviewCreateEditContainer

dc8f88370c139b4e26fd934c7d16d7144a98a267
evan miller <emille4@clemson.edu>
2017-03-31 13:53:51 -0400

    changed salary number to have zero decimal places

--- app/scripts/components/degree_detail.jsx
+++ app/scripts/components/degree_detail.jsx
@@ -18,8 +18,8 @@ class DegreeDetail extends React.Component {
     console.log('det', this.props.ascData)
     var difference = parseInt(this.props.bacAverage) - parseInt(this.props.ascAverage);
     var difAfterTen = difference * 10;
-    var associateAvg = parseInt(this.props.ascAverage).toFixed(2);
-    var bachelorsAvg = parseInt(this.props.bacAverage).toFixed(2);
+    var associateAvg = parseInt(this.props.ascAverage).toFixed(0);
+    var bachelorsAvg = parseInt(this.props.bacAverage).toFixed(0);
 
     return(
       <div className="row">
